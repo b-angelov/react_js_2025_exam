890c9eb29176dde1f27eaab4b2eaeabb9165577b
b-angelov <sauronthegreat76@gmail.com>
2025-04-06 12:06:34 +0300

    fix: calendar week improvements

--- src/components/calendar/ListItem.jsx
+++ src/components/calendar/ListItem.jsx
@@ -2,8 +2,8 @@ export default function ListItem(props){
     const {saint,feast,week_day} = props
 
     const feastsAndSaints = () =>{
-        const saints = saint.map(e=>(<><span>{`${e.name.slice(0, 20)}...`}</span></>))
-        const feasts = feast.map(e=>(<span>{`${e.name.slice(0, 20)}...`}</span>))
+        const saints = saint && saint.map(e=>(<><span>{`${e.name.slice(0, 20)}...`}</span></>))
+        const feasts = feast && feast.map(e=>(<span>{`${e.name.slice(0, 20)}...`}</span>))
         return (
             <>
                 <sub style={{color: "red", "font-style":"italic", "text-transform":"capitalize", "top":"-7em", position:"relative"}}>{week_day}</sub>
