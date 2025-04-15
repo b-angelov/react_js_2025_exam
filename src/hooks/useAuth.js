9a661bffebe2a84767247d9cfcf23a8bb779fcea
b-angelov <sauronthegreat76@gmail.com>
2025-04-15 08:27:09 +0300

    fix: updated auth flow

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -15,34 +15,11 @@ export default function useAuth()  {
     const login = (username, password, messageCallback=()=>{})=>{
         let response;
 
-        console.log(token)
 
         if(username && password){
-            // fetch(`${import.meta.env.VITE_API_URL}/api/token/`, {})
-            // response = fetch(`${import.meta.env.VITE_API_ADDRESS}api/token/`,{
-            //     method: 'POST',
-            //     headers:{
-            //         'Content-Type': 'application/json',
-            //         'Accept': 'application/json'
-            //     },
-            //     credentials: 'include',
-            //     body: JSON.stringify({
-            //         username: username,
-            //         password: password
-            //     })
-            // })
-            //     .then(response => response.json())
-            //     .then((response) => {
-            //     setToken(response.access);
-            //     return {status: 200, data: response};
-            // }).catch((error) => {
-            //     console.error("Error logging in:", error);
-            //     return {status: 500, error}
-            // });
             response = api.post(`/api/token/`,{username,password}).
             then(response=>{
                 setToken(response.data.access);
-                console.log(jwtDecode(response.data.access))
                 setUser(jwtDecode(response.data.access))
                 toggleSuccess(response.status === 200, "Влизането e успешно!", "Влизането e неуспешно!", messageCallback)
                 return {status: 200, data: response};
@@ -52,30 +29,9 @@ export default function useAuth()  {
                 return {status: 500, error}
             })
         } else{
-            // response = fetch(`${import.meta.env.VITE_API_ADDRESS}api/token/refresh/`,{
-            //     method: 'POST',
-            //     headers:{
-            //         'Content-Type': 'application/json',
-            //         'Accept': 'application/json',
-            //     },
-            //     credentials: 'include',
-            //     body: JSON.stringify({
-            //         username: username,
-            //         password: password
-            //     })
-            // })
-            //     .then(response => response.json())
-            //     .then((response) => {
-            //     setToken(response.access);
-            //     return {status: 200, data: response};
-            // }).catch((error) => {
-            //     console.error("Error logging in:", error);
-            //     return {status: 500, error}
-            // })
             response = api.post(`/api/token/refresh/`).
             then(response=>{
                 setToken(response.data.access);
-                console.log(jwtDecode(response.data.access))
                 setUser(jwtDecode(response.data.access))
                 return {status: 200, data: response};
             }).catch((error) => {
